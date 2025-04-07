ae12a765ca282650b646b2f92cbe8da0075cfff3
Christophar Antony <christopharantony@gmail.com>
2025-04-07 18:09:29 +0530

    Install: ESLint & Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import importPlugin from 'eslint-plugin-import'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-config-prettier'
 
 export default [
   { ignores: ['dist'] },
@@ -17,17 +21,28 @@ export default [
       },
     },
     plugins: {
+      react,
+      'jsx-a11y': jsxA11y,
+      import: importPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
+
+  prettier,
 ]
